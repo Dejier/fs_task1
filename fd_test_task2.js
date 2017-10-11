f4a9ebfbb417d1a404cf4b55ea58c381e345efaf
Dejier <sergshekhirev@gmail.com>
2017-10-11 21:58:37 +0300

    task2 switch

--- fd_test_task2.js
+++ fd_test_task2.js
@@ -1,9 +1,27 @@
 'use strict'
 
 function assess(f, data) {
-
+	let n, lnn, n2, n3;
+	let t0 = perfomance.now();
 	f.apply(null, data); 
 	//f(...data);
+	let t1 = perfomance.now();
+
+
+	switch(t1-t0) {
+		case n: 
+			return "O(n)";
+			break;
+		case lnn: 
+			return "O(log(n))";
+			break;
+		case n2:
+			return "O(n2)";
+			break;
+		case n3: 
+			return "O(n3)";
+			break;
+	}
 
 }
 
