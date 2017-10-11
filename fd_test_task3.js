a6ed2754ed6bdcfd809089ffbbe2601dc6591a80
Dejier <sergshekhirev@gmail.com>
2017-10-11 22:11:03 +0300

    task3 edit

--- fd_test_task3.js
+++ fd_test_task3.js
@@ -9,14 +9,14 @@ module.exports = function (red, blue) {
 		let diff = red[curRedBottle] - blue[curBlueBottle];
 		if (diff > 0) {
 			red[curRedBottle] -=  blue[curBlueBottle];
-			arr.push([curRedBottle, blue[curBlueBottle]]);			
+			arr.push([curBlueBottle, curRedBottle, blue[curBlueBottle]]);			
 			curBlueBottle++;
 		} else if(diff < 0) {
 			blue[curBlueBottle] -= red[curRedBottle];
-			arr.push([curRedBottle, red[curRedBottle]]);
+			arr[curBlueBottle].push([curBlueBottle, curRedBottle, red[curRedBottle]]);
 			curRedBottle++;
 		} else {
-			arr.push([curRedBottle, blue[curBlueBottle]]);
+			arr.push([curBlueBottle, curRedBottle, blue[curBlueBottle]]);
 			curRedBottle++;
 			curBlueBottle++;
 		}
